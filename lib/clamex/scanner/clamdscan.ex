58bf61f0f44fa771eb2506b60eb785d79b9df392
Michał Szajbe <michal.szajbe@gmail.com>
2018-11-20 20:14:44 +0100

    Improved scanners' documentation

--- lib/clamex/scanner/clamdscan.ex
+++ lib/clamex/scanner/clamdscan.ex
@@ -5,14 +5,34 @@ defmodule Clamex.Scanner.Clamdscan do
 
   @behaviour Clamex.Scanner
 
-  @default_args ["--no-summary"]
+  @doc """
+  Perform file scan using `clamdscan` command-line utility.
 
+  ## Examples
+
+      iex> Clamex.Scanner.Clamdscan.scan("test/files/virus.txt")
+      {:error, :virus_found}
+
+      iex> Clamex.Scanner.Clamdscan.scan("test/files/safe.txt")
+      :ok
+
+  ## Error reasons
+
+  * `:virus_found` - file is infected
+  * `:cannot_access_file` - file specified as `path` cannot be accessed
+  * `:scanner_not_available` - scanner is not available at `executable_path`
+  * `:cannot_connect_to_clamd` - ClamAV daemon is not running in background
+  * any other error reported by the scanner will be returned as is (as String)
+
+  """
+  @callback scan(path :: Path.t()) ::
+              :ok | {:error, atom()} | {:error, String.t()}
   def scan(path) do
     try do
       {output, exit_code} =
         System.cmd(
           "clamdscan",
-          @default_args ++ [path],
+          ["--no-summary", path],
           stderr_to_stdout: true
         )
 
--- lib/clamex/scanner/clamscan.ex
+++ lib/clamex/scanner/clamscan.ex
@@ -5,14 +5,33 @@ defmodule Clamex.Scanner.Clamscan do
 
   @behaviour Clamex.Scanner
 
-  @default_args ["--no-summary"]
+  @doc """
+  Perform file scan using `clamscan` command-line utility.
 
+  ## Examples
+
+      iex> Clamex.Scanner.Clamscan.scan("test/files/virus.txt")
+      {:error, :virus_found}
+
+      iex> Clamex.Scanner.Clamscan.scan("test/files/safe.txt")
+      :ok
+
+  ## Error reasons
+
+  * `:virus_found` - file is infected
+  * `:cannot_access_file` - file specified as `path` cannot be accessed
+  * `:scanner_not_available` - scanner is not available at `executable_path`
+  * any other error reported by the scanner will be returned as is (as String)
+
+  """
+  @callback scan(path :: Path.t()) ::
+              :ok | {:error, atom()} | {:error, String.t()}
   def scan(path) do
     try do
       {output, exit_code} =
         System.cmd(
           "clamscan",
-          @default_args ++ [path],
+          ["--no-summary", path],
           stderr_to_stdout: true
         )
 
